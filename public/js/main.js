5e6a77e3237722e179124c74f1f88ee9a0eabe12
Raphaël Bacco <raphael@bacco.fr>
2023-06-20 10:24:54 +0200

    MAJ pwa

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,11 @@
-// PWA
-// https://www.pwabuilder.com
-// This is the "Offline page" service worker
-
-console.log("Hi! I'm MRPT!");
-
 if ("serviceWorker" in navigator) {
-    if (navigator.serviceWorker.controller) {
-        console.log("[PWA Builder] active service worker found, no need to register");
-    } else {
-        // Register the service worker
+    window.addEventListener("load", function() {
         navigator.serviceWorker
-            .register("/pwabuilder-sw.js", {
-                scope: "/"
-            })
-            .then(function (reg) {
-                console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
-//                    reg.periodicSync.register('content-sync', {
-//                        // An interval of one day.
-//                        minInterval: 5 * 60 * 1000,
-//                    });
-//                    console.log("register('content-sync')");
-                }
-            );
-
-//        self.addEventListener('periodicsync', (event) => {
-//            if (event.tag === 'content-sync') {
-//                console.log("event('content-sync')");
-//                // See the "Think before you sync" section for
-//                // checks you could perform before syncing.
-//                event.waitUntil(
-//                    navigator.setAppBadge(Math.floor(Math.random()*100))
-//                );
-//            }
-//        });
-
-    }
+            .register("/pwabuilder-sw.js")
+            .then(res => console.log("[OK] Service worker registered"))
+            .catch(err => console.log("[ERR] Service worker not registered", err))
+    })
 }
 else {
-    console.log("[PWA Builder] Navigator not serviceWorker compatible");
-    console.log(navigator);
+    console.log('[WARNING] Service worker is not supported.');
 }
